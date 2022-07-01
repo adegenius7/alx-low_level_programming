77dd0b2d99b21d36e15216abeb6c97d1efbf9b10
SHOBANKE ADEWOLE USMAN <99494269+adegenius7@users.noreply.github.com>
2022-07-01 05:38:51 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,12 @@
-#include<stdio.h>
-/*
+#include <stdio.h>
+
+/**
+ * main - prints a sentence with the printf function
  *
- *
- * main -is where program starts from
- *
- *Return: 0
+ * Return: returns 0 after printing
  */
-
-
 int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return(0);
+	return (0);
 }

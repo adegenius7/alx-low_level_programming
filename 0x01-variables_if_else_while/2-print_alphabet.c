286f17067af1796d3aacd83ee9adde4c1bb00758
adegenius7 <adegenius007@gmail.com>
2022-07-01 21:46:04 +0000

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - where program starts
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}

3db78e751e5700fd2fef3dbcb9078f41e062864a
adegenius7 <adegenius007@gmail.com>
2022-07-05 23:25:39 +0000

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - where the program starts
+ *
+ *
+ * Return: On success 1.
+ * on error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - where program starts
+ * my function is in my header file
+ *
+ * Return: 0 alwyas as success
+ *
+ */
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}

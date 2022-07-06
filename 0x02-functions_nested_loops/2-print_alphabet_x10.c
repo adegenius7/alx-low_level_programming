5ca1bfa011a9d398f3ccafe5c355bb891af061ca
adegenius7 <adegenius007@gmail.com>
2022-07-06 00:25:46 +0000

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *print_alphabet_x10 -prints alphabet 10 times
+ *
+ *Return value not specified in void
+ *
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	char a;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar (a);
+	}
+	_putchar ('\n');
+}

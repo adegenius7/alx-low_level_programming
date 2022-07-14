66eb1091c9a2f93d3c4b7c54ec1193c114b53b3a
adegenius7 <adegenius007@gmail.com>
2022-07-14 21:10:19 +0000

    strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,11 +14,16 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 	j = 0;
 
-	while (dest[i] != '\0' && src[j] != '\0')
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		i++;
 		j++;
+		i++;
 	}
-return (dest);
+	dest[i] = '\0';
+	return (dest);
 }

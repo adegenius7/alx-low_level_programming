47395156ceefd5c0c95d3c607eb41d3927fe96d7
adegenius7 <adegenius007@gmail.com>
2022-07-14 20:39:53 +0000

    strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - check the code
+ *@dest: destination string
+ *@src: source string
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *dest, dest[], src[];
+
+	char *src;
+
+	int i, j;
+
+	len1 = _strlen(dest);
+
+	len2 = _strlen(src);
+
+	for (i = 0; i <= len2; i++)
+	{
+		dest[len1 + i] = src[i]
+	}
+	return (dest);
+}

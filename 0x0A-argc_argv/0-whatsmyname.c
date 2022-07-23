eef7ab3e60b4225a8cc51e3286ff493553420374
adegenius7 <adegenius007@gmail.com>
2022-07-23 02:42:48 +0000

    arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
- * main(int argc, char* argv[])- where the programs looks forthe commands passed
- *
- * Return 0
+ * main(int argc, char* argv[])- where the programs looks
+ * @argc: number of arguments
+ *@argv: argument vector
+ * Return:Always 0
  *
  */
 
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	printf("My name is %s", argv[0]);
 	(void)argc;
 
-		return 0;
+		return (0);
 }

04ece334caf74a6923a80ee4bfab32e359e89933
adegenius7 <adegenius007@gmail.com>
2022-07-23 04:30:45 +0000

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- progarm starts
+ *@argc: number of commands
+ *@argv: argument vector
+ *Return: Always success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main- progarm starts here
+ *@argc: number of commands
+ *@argv: place holder for commands
+ *Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+		printf("%d\n", argc);
+
+	return (0);
+}

b298eb6075f1977c533a1447ad61eeb285224396
adegenius7 <adegenius007@gmail.com>
2022-07-26 01:12:40 +0000

    add

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_array- is the address of the first character
+ * @size: size of array
+ * @c: character
+ * Return: success
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *s;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	s = (char *)malloc(size * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+		return (s);
+	}
+}

830489bdd12ff3021fc11919ef47405a7d952680
adegenius7 <adegenius007@gmail.com>
2022-08-01 20:54:22 +0000

    add

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,8 @@
+/**
+ *struct dog - struct that stores some information of a dog
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ */
+
 struct dog {char *name; float age; char *owner;};

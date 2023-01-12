5a91cf01c0d1fe2d0c839b96bbc91f121e2a0c62
adegenius7 <adegenius007@gmail.com>
2023-01-12 17:07:30 +0000

    dlist

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ *struct dlistint_s - doubly linked list
+ *@n: integer
+ *@prev: points to the previous node
+ *@next: points to the next node
+ *
+ *Description: doubly linked list node structure
+ *
+ **/
+
+
+
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+int count;
+dlistint_t *temp = dlistint_t *h;
+while (temp->prev == NULL && temp->next != NULL)
+{
+	count++;
+	temp = temp->next;
+}
+return count;

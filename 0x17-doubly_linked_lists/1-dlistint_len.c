0a1e4d6bebce509e95d4624f327fba88f64ec476
adegenius7 <adegenius007@gmail.com>
2023-01-12 19:09:53 +0000

    prints the elements in a node

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ *dlistint_len- prints the number of elements of a list
+ *
+ *@h: the pointer to the head of a list
+ *
+ *Return: number of elements
+ **/
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+	while (h->prev != NULL)
+	{
+		h = h->prev
+	}
+	while (h->next != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}

442dc11d7d2321a1490d4ef32983d865ca17c8bb
SHOBANKE ADEWOLE USMAN <99494269+adegenius7@users.noreply.github.com>
2023-01-14 05:08:49 +0300

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,5 +7,15 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
+	
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
 
 }

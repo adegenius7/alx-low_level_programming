b43b01c5cc149f1c533111f08fdaf2c08ca3692a
adegenius7 <adegenius007@gmail.com>
2023-01-14 01:09:44 +0000

    delete a node at the end of a doubly linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,15 +8,11 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
 	dlistint_t *temp2;
-
+	
 	temp = head;
-
-	if (temp != NULL)
-	{
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp2 = temp->prev;
-	}
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp2 = temp->prev;
 	temp2->next = NULL;
 	free(temp);
 }
